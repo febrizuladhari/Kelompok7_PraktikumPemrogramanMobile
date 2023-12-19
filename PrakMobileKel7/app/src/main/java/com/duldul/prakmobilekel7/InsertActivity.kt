733c3b4d430ca4f9f9eeff25c2b08f686084facb
Febri Zuladhari <febrizuladhari12345@gmail.com>
2023-12-19 13:30:01 +0700

    add activities

---
+++ PrakMobileKel7/app/src/main/java/com/duldul/prakmobilekel7/InsertActivity.kt
@@ -0,0 +1,11 @@
+package com.duldul.prakmobilekel7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert)
+    }
+}
